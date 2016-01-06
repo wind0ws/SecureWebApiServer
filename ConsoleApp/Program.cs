3eaa788fd32121aecffd63296cd4f9d368d70ec2
Threshold <hsjfox@foxmail.com>
2016-01-06 12:38:18 +0800

    Add Packages Config and Screen shot

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,8 +11,8 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             //Utils.RandomUtil.Run();
-            //AppKeySecret.Run();
-            // Client.Run();
+            AppKeySecret.Run();
+             //Client.Run();
            
             Console.WriteLine("\r\n Console App运行结束，按回车键退出程序。");
             Console.ReadLine();
